1240423334cb136ad08caabb8346a6366221aa22
okuma <triple_55@yahoo.co.jp>
2017-08-07 08:16:49 +0900

    不要モジュール削除

--- sample9/lib/db.rb
+++ sample9/lib/db.rb
@@ -3,12 +3,12 @@
 # and open the template in the editor.
 require "sequel"
 
-DB = Sequel.connect('jdbc:oracle:thin:triple/triple@//localhost:1521/triple_pb.co.jp')
+##DB = Sequel.connect('jdbc:oracle:thin:triple/triple@//localhost:1521/triple_pb.co.jp')
 
 ##テーブル情報を表示する
-def fetchUserTables 
-  DB.fetch("select table_name from user_tables ")
-  .all do
-    |x|puts x 
-  end
-end
+##def fetchUserTables 
+##  DB.fetch("select table_name from user_tables ")
+##  .all do
+##    |x|puts x 
+##  end
+##end
